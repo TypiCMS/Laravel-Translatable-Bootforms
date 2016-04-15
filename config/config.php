020c394a04d658b62e36529ab7c4e89ba5179c1c
Samuel De Backer <sdebacker@gmail.com>
2016-04-15 19:03:11 +0200

    locales in config

--- config/config.php
+++ config/config.php
@@ -4,17 +4,27 @@ return [
 
 	/*
 	|--------------------------------------------------------------------------
+	| Locales
+	|--------------------------------------------------------------------------
+	|
+	| Array of available locales
+	|
+	*/
+	'locales' => ['en'],
+
+	/*
+	|--------------------------------------------------------------------------
 	| Label override
 	|--------------------------------------------------------------------------
 	|
 	| The label to use when creating a localized form element.
 	| Useful for indicating that the form element is meant for a certain language.
 	| To have the label itself translated you still need to pass a trans() value as label in the view.
-	| 
+	|
 	| Available placeholders:
 	|   %label   The original (eventually translated) label.
 	|   %locale  The locale  (e.g. 'en')
-	| 
+	|
 	| E.g. for '%label (%locale)' and TranslatableBootForm::text(trans('nameTranslationKey'), 'name')
 	| this would yield the following labels:
 	|   <label for="en[name]">Name (en)</label>
